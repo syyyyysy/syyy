e8dbb395fe2e33604054c2bfd7295e2dd3158a15
syyyyysy <111685323+syyyyysy@users.noreply.github.com>
2022-08-23 23:51:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
+birthday2 = os.environ['BIRTHDAY2']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
-user_id = os.environ["USER_ID"]
+user_ids = os.environ["USER_ID"].split("\n")
 template_id = os.environ["TEMPLATE_ID"]
 
 
@@ -22,7 +23,7 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  return weather['weather'], math.floor(weather['temp']), math.floor(weather['high']), math.floor(weather['low'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -33,6 +34,21 @@ def get_birthday():
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
+
+def get_birthday2():
+  next = datetime.strptime(str(date.today().year) + "-" + birthday2, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+def get_wea_war():
+  tem_wea,tem_a,tem_b,tem_c=get_weather()
+  if str(tem_wea) == "晴":
+    return "宝今天天气好，出去记得防晒噢"
+  else str(tem_wea) == "阴":
+    return "今天可以出去走走噢宝~"
+  else :
+    return "今天可能会下雨哎，宝记得带伞呢"
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
@@ -47,7 +63,26 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+wea, temperature, highest, lowest = get_weather()
+now_year = today.year
+now_month = todey.month
+now_day = today.day
+data = {"weather_warning":{"value": get_wea_war()},
+        "date_D":{"value":now_day},
+        "date_M":{"value":now_month},
+        "date_Y":{"value":now_year},
+        "weather":{"value":wea},
+        "temperature":{"value":temperature},
+        "love_days":{"value":get_count()},
+        "birthday_left":{"value":get_birthday(), "color":get_random_color()},
+        "birthday_left2":{"value":get_birthday2(), "color":get_random_color()},
+        "words":{"value":get_words(), "color":get_random_color()},
+        "highest":{"value":highest, "color":get_random_color()},
+        "lowest":{"value":lowest,"color":get_random_color()}
+        }
+count = 0
+for user_id in user_ids:
+  res = wm.send_template(user_id, template_id, data)
+  count+=1
+
+print("发送了" + str(count) + "条消息")
